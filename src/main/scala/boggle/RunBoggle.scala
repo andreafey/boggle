7d9fb35ff4746dda76f127cee368a59739c3ce8c
Andrea Fey <andrea.github@mailnull.com>
2014-02-12 15:14:00 -0500

    add list member tester

--- src/main/scala/boggle/RunBoggle.scala
+++ src/main/scala/boggle/RunBoggle.scala
@@ -33,11 +33,12 @@ object RunBoggle {
       
     }
     
+    
     def findWords(b:Board, dict:LetterTree):List[String] = {
       val letters = for {
 		x:Int <- Range(0,b.boardsize)
 		y:Int <- Range(0,b.boardsize)
-	  } yield (x,y)
+	  } yield (x,y) 
 	  val all = for {
 	    (x,y) <- letters
 	  } yield findWordsHelper(b, dict, List(), List((x,y)))
@@ -50,13 +51,15 @@ object RunBoggle {
       
     }
     
+    def isInList(item:(Int,Int), l:List[(Int,Int)]):Boolean = l match {
+    	case Nil => false
+    	case x :: xs => if (item == x) true else isInList(item,xs)
+    }
     def adjacentCoords(b:Board, coord:(Int,Int), usedCoords:List[(Int, Int)]):List[(Int, Int)] = {
-      val all = for {
+      for {
         x:Int <- Range(coord._1-1, coord._1+1) if x >=0 && x < b.boardsize
         y:Int <- Range(coord._2-1, coord._2+1) if y >=0 && y < b.boardsize
-      } yield (x, y) if (x, y) ! usedCoords.toArray.contai
-      
-      all.toList.filter()
+      } yield (x, y) if isInList((x,y), usedCoords)
     }
     
 }
